37f23728f6bb3d204a526c53df7818dc6eb9afd9
xinshuo <xinshuo.weng@gmail.com>
2017-03-14 15:02:40 -0400

    update

--- file_io/testgetpoint (xinshuo's conflicted copy 2017-03-14).m
+++
@@ -1,5 +0,0 @@
-close all;
-clc;
-clear;
-
-getPointFromImage('D:\Dropbox (Facebook)\dani_exagerratedtones_100_199\cam330014\image0109.png', 1, 'D:\oculus\NewAnnotationTool\AnnotationTool_Oculus\KeypointToMesh\testfortriangulationnew\predictions\camera330014\00000.pose');
