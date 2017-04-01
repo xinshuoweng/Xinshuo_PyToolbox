493c390247cd090b2c8f50a1a8ec728634df849d
xinshuoweng <xinshuo.weng@gmail.com>
2017-04-01 19:58:34 -0400

    update structure

--- ml/deep_learning/layer.py
+++ ml/deep_learning/layer.py
@@ -9,10 +9,24 @@ class AbstractLayer(object):
 	'''
 	define an abstract layer for all type of layers
 	'''
-	def __init__(self, name):
+	def __init__(self, name, datatype=None, paramtype=None):
+		if datatype is not None:
+			assert any(datatype == item for item in ['uint', 'single', 'double']), 'type of data should be one of ''uint8'' ''single'' ''double'' '
+		else:
+			datatype = 'single'
+			print 'datatype of the layer is not defined. By default, we use single floating point to save the datas'
+		if paramtype is not None:
+			assert any(paramtype == item for item in ['uint', 'single', 'double']), 'type of parameter should be one of ''uint8'' ''single'' ''double'' '
+		else:
+			paramtype = 'single'
+			print 'paramtype of the layer is not defined. By default, we use single floating point to save the parameter'
 		assert isstring(name), 'the name of input layer should be a string'	
 		self._name = name
-	
+		# self._data = None
+		# self._params = None
+		self._datatype = datatype
+		self._paramtype = paramtype
+
 	@property
 	def name(self):
 		return self._name
@@ -21,21 +35,21 @@ class AbstractLayer(object):
 	def name(self, name):
 		assert isstring(name), 'the name of a layer should be a string'
 
-	@property
-	def data(self):
- 		return self._data
+	# @property
+	# def data(self):
+ # 		return self._data
 
-	@data.setter
-	def data(self, data):
- 		raise NotImplementedError
+	# @data.setter
+	# def data(self, data):
+ # 		raise NotImplementedError
 
- 	@property
-	def params(self):
- 		return self._params
+ # 	@property
+	# def params(self):
+ # 		return self._params
 
-	@params.setter
-	def params(self, params):
- 		raise NotImplementedError
+	# @params.setter
+	# def params(self, params):
+ # 		raise NotImplementedError
 
 	@property
 	def paramtype(self):
@@ -84,22 +98,18 @@ class Input(AbstractLayer):
 	'''
 	define an input layer which contains info about the input data
 	'''
-	def __init__(self, name):
-		super(Input, self).__init__(name)
+	def __init__(self, name, datatype=None, paramtype=None):
+		super(Input, self).__init__(name=name, datatype=datatype, paramtype=paramtype)
 		# assert isinstance(data, np.ndarray), 'the input data layer should contains numpy array'
-		self._data = None
-		self._params = None
-		self._paramtype = None
-		self._datatype = None
-	
-	@AbstractLayer.data.setter
-	def data(self, data):
-		assert isinstance(data, np.ndarray), 'the input data layer should contains numpy array'
-		self._data = data
+		# self._data = data
+	# @AbstractLayer.data.setter
+	# def data(self, data):
+	# 	assert isinstance(data, np.ndarray), 'the input data layer should contains numpy array'
+	# 	self._data = data
 
-	@AbstractLayer.params.setter
-	def params(self, params):
-		assert False, 'No parameter can be set in the input layer'
+	# @AbstractLayer.params.setter
+	# def params(self, params):
+	# 	assert False, 'No parameter can be set in the input layer'
 
 	@property
 	def type(self):
@@ -108,21 +118,23 @@ class Input(AbstractLayer):
  	def get_num_param(self):
  		return 0
 
- 	def get_output_blob_shape(self):
- 		return self._data.shape
+ 	def get_output_blob_shape(self, data):
+ 		assert isinstance(data, np.ndarray), 'the input data layer should contains numpy array'
+ 		return data.shape
 
 class Layer(AbstractLayer):
 	'''
 	define necessary layer parameter and property for deep learning
+	parameters are following HxW format
 	'''
-	def __init__(self, name, nInputPlane, nOutputPlane, kernal_size=None, stride=None, padding=None, datatype=None, params=None, paramtype=None):
-		super(Layer, self).__init__(name)
+	def __init__(self, name, nInputPlane, nOutputPlane, kernal_size=None, stride=None, padding=None, datatype=None, paramtype=None):
+		super(Layer, self).__init__(name=name, datatype=datatype, paramtype=paramtype)
 		assert type(nInputPlane) is int and nInputPlane > 0, 'number of input channel is not correct'
 		assert type(nOutputPlane) is int and nOutputPlane > 0, 'number of output channel is not correct'
 		assert kernal_size is None or type(kernal_size) is int or len(kernal_size) == 2, 'kernal size is not correct'
 		assert stride is None or type(stride) is int or len(stride) == 2, 'stride size is not correct'
 		assert padding is None or type(padding) is int or len(padding) == 2, 'padding size is not correct'
-		assert params is None or isinstance(params, np.ndarray), 'parameter is not correct'
+		# assert params is None or isinstance(params, np.ndarray), 'parameter is not correct'
 
 		if type(kernal_size) is not int and kernal_size is not None:
 			assert all(item > 0 and type(item) is int for item in kernal_size), 'kernal size must be positive integer'
@@ -130,17 +142,6 @@ class Layer(AbstractLayer):
 			assert all(stride > 0 and type(item) is int for item in stride), 'stride must be positive integer'
 		if type(padding) is not int and padding is not None:
 			assert all(padding >= 0 and type(item) is int for item in padding), 'padding must be non-negative integer'
-		if datatype is not None:
-			assert any(datatype == item for item in ['uint', 'single', 'double']), 'type of data should be one of ''uint8'' ''single'' ''double'' '
-		else:
-			datatype = 'single'
-			print 'datatype of the layer is not defined. By default, we use single floating point to save the datas'
-
-		if paramtype is not None:
-			assert any(paramtype == item for item in ['uint', 'single', 'double']), 'type of parameter should be one of ''uint8'' ''single'' ''double'' '
-		else:
-			paramtype = 'single'
-			print 'paramtype of the layer is not defined. By default, we use single floating point to save the parameter'
 
 		# set horizontal and vertical parameter as the same if only one dimentional input is obtained
 		if type(kernal_size) is int:
@@ -155,10 +156,7 @@ class Layer(AbstractLayer):
 		self._padding = padding
 		self._nInputPlane = nInputPlane
 		self._nOutputPlane = nOutputPlane
-		self._datatype = datatype
-		self._paramtype = paramtype
-		self._data = None
-		self._params = params
+		# self._params = params
 
 	@property
 	def nInputPlane(self):
@@ -184,26 +182,31 @@ class Convolution(Layer):
 	'''
 	define a 2d convolutional layer
 	'''
-	def __init__(self, name, nInputPlane, nOutputPlane, kernal_size, stride=None, padding=None, datatype=None, params=None, paramtype=None):
-		super(Convolution, self).__init__(name, nInputPlane, nOutputPlane, kernal_size, stride, padding, datatype, params, paramtype)
+	def __init__(self, name, nInputPlane, nOutputPlane, kernal_size, stride=None, padding=None, datatype=None, paramtype=None):
+		super(Convolution, self).__init__(name=name, nInputPlane=nInputPlane, nOutputPlane=nOutputPlane, kernal_size=kernal_size, 
+			stride=stride, padding=padding, datatype=datatype, paramtype=paramtype)
+		# assert params.ndim == 3, 'the parameter of convolution layer should be 3-d array'
+		# assert params.shape(0) == self.nInputPlane, 'first dimension of parameter in convolution layer is not correct'
+		# self._params = params
 		if stride is None:
 			stride = (1, 1)
 		if padding is None:
 			padding = (0, 0)
 
-	@AbstractLayer.data.setter
-	def data(self, data):
-		assert isinstance(data, np.ndarray), 'the data of convolution layer should contains numpy array'
-		assert data.ndim == 4, 'the data of convolution layer should be 4-d array'
-		assert data.shape(3) == self.nOutputPlane, 'last dimension of data in convolution layer is not correct'
-		self._data = data
+	# @AbstractLayer.data.setter
+	# def data(self, data):
+	# 	assert isinstance(data, np.ndarray), 'the data of convolution layer should contains numpy array'
+	# 	assert data.ndim == 4, 'the data of convolution layer should be 4-d array'
+	# 	assert data.shape(3) == self.nOutputPlane, 'last dimension of data in convolution layer is not correct'
+	# 	self._data = data
 
-	@AbstractLayer.params.setter
-	def params(self, params):
-		assert isinstance(params, np.ndarray), 'the parameter of convolution layer should contains numpy array'
-		assert params.ndim == 3, 'the parameter of convolution layer should be 3-d array'
-		assert params.shape(0) == self.nInputPlane, 'first dimension of parameter in convolution layer is not correct'
-		self._params = params
+	# @AbstractLayer.params.setter
+	# def params(self, params):
+	# 	assert isinstance(params, np.ndarray), 'the parameter of convolution layer should contains numpy array'
+	# 	assert params.ndim == 4, 'the parameter of convolution layer should be 3-d array'
+	# 	assert params.shape(0) == self.nInputPlane, 'first dimension of parameter in convolution layer is not correct'
+	# 	assert params.shape() == self.nInputPlane, 'first dimension of parameter in convolution layer is not correct'
+	# 	self._params = params
 
 	@property
 	def type(self):
--- ml/deep_learning/test_layer.py
+++ ml/deep_learning/test_layer.py
@@ -11,32 +11,50 @@ def test_Input():
 	assert inputlayer.name is 'data'
 	assert inputlayer.type is 'Input'
 	assert_allclose(inputlayer.get_num_param(), 0)
+	assert inputlayer.datatype is 'single'
+	assert inputlayer.paramtype is 'single'
+	# assert inputlayer.data is None
+	# assert inputlayer.params is None
 
-	assert inputlayer.data is None
+	inputlayer = Input(name='data', datatype='double')
+	assert inputlayer.datatype is 'double'
+
+	inputlayer = Input(name='data', datatype='double', paramtype='uint')
+	assert inputlayer.paramtype is 'uint'
+
+	# inputlayer.data = data
+	# assert_allclose(inputlayer.data, data)
 	data = np.array((100, 100, 3), dtype='float32')
-	inputlayer.data = data
-	assert_allclose(inputlayer.data, data)
-	assert_allclose(inputlayer.get_output_blob_shape(), data.shape)
+	assert_allclose(inputlayer.get_output_blob_shape(data), data.shape)
 
 
-def Test_Convolution():
-	convlayer = Convolution(name='conv1', nInputPlane=3, nOutputPlane=512, kernal_size=3)
+def test_Convolution():
+	# parameter shape test
+	convlayer = Convolution(name='conv1', nInputPlane=3, nOutputPlane=512, kernal_size=(3,4), stride=2)
+	assert_allclose(convlayer.kernal_size, (3, 4))
+	assert_allclose(convlayer.stride, (2, 2))
+	convlayer = Convolution(name='conv1', nInputPlane=3, nOutputPlane=512, kernal_size=3, stride=(3,4), padding=1)
+	assert_allclose(convlayer.stride, (3, 4))
+	assert_allclose(convlayer.padding, (1, 1))
+	params = np.ndarray(())
+	convlayer = Convolution(name='conv1', nInputPlane=3, nOutputPlane=512, kernal_size=3, stride=2, padding=(3,4))
+	assert_allclose(convlayer.padding, (3, 4))
+
+	# main test
+	convlayer = Convolution(name='conv1', nInputPlane=3, nOutputPlane=512, kernal_size=(3, 4), stride=(2, 3), padding=(1, 2))
 	assert convlayer.name is 'conv1'
 	assert_allclose(convlayer.nInputPlane, 3)
 	assert_allclose(convlayer.nOutputPlane, 512)
-	assert_allclose(convlayer.kernal_size, (3, 3)) 
-	assert_allclose(convlayer.stride, 1)
-	assert_allclose(convlayer.padding, 0)
+	assert_allclose(convlayer.kernal_size, (3, 4)) 
+	assert_allclose(convlayer.stride, (2, 3))
+	assert_allclose(convlayer.padding, (1, 2))
 	assert convlayer.datatype is 'single'
 	assert convlayer.paramtype is 'single'
-	assert convlayer.params is None
-	
-	convlayer = Convolution(name='conv1', nInputPlane=3, nOutputPlane=512, kernal_size=3, )
-convlayer = Convolution(name='conv1', nInputPlane=3, nOutputPlane=512, kernal_size=3)
-convlayer = Convolution(name='conv1', nInputPlane=3, nOutputPlane=512, kernal_size=3)
-convlayer = Convolution(name='conv1', nInputPlane=3, nOutputPlane=512, kernal_size=3)
-
-
+	# assert convlayer.params is None
+	# assert convlayer.data is None
+	assert_allclose(convlayer.get_num_param(), 3*3*4*512)
+	bottom_shape = (256, 128, 3)
+	assert_allclose(convlayer.get_output_blob_shape(), )
 
 if __name__ == '__main__':
     pytest.main([__file__])
