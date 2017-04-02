5d8a22fb3ae1d05ac866b82d8635df748b61b26e
xinshuoweng <xinshuo.weng@gmail.com>
2017-04-02 15:51:20 -0400

    update summary info for network

--- machine_learning/deep_learning/net.py
+++ machine_learning/deep_learning/net.py
@@ -1,10 +1,13 @@
 # Author: Xinshuo Weng
 # email: xinshuo.weng@gmail.com
+
+
 from collections import OrderedDict
 from operator import mul
 import humanize
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+import os
 
 import __init__paths__
 from type_check import isstring
@@ -114,90 +117,6 @@ class SequentialNet(Net):
 		assert input_data.shape[0] > 0, 'batch size must be positive'
 		self._batch_size = input_data.shape[0]
 
-	def summary(self, savepath=None):
-		assert self._compiled, 'the network is not compiled'
-		assert savepath is None or is_path_exists_or_creatable(savepath), 'save path is not correct'
-		# print terminal network info
-		print('=======================================================================================================')
-		print('{:<30}{:<25}{:<15}{:<20}'.format('Layer (type)', 'Output Shape', 'Param', 'Memory Usage(data, param)'))
-		print('-------------------------------------------------------------------------------------------------------')
-		total_param = 0
-		memory_data_list = dict()
-		memory_param_list = dict()
-		previous_layer_shape = None
-		for layer_name in self._blobs.keys():
-			layer = self._layers[layer_name]
-			output_shape = self._blobs[layer_name]['data'].shape
-			layer_num_param = layer.get_num_param(previous_layer_shape)
-			memory_data = self.get_memory_usage_data_layer(layer_name)
-			memory_param = layer.get_memory_usage_param(previous_layer_shape)
-			memory_data_list[layer_name] = memory_data
-			memory_param_list[layer_name] = memory_param
-			memory = memory_data + memory_param
-			memory_data_format = humanize.naturalsize(memory_data, binary=True)
-			memory_param_format = humanize.naturalsize(memory_param, binary=True)
-			memory_format = humanize.naturalsize(memory, binary=True)
-			print('{:<30}{:<25}{:<15}{:<20}'.format('%s (%s)' % (layer_name, layer.type), '(%d, %d, %d, %d)' % (self._batch_size, output_shape[0], output_shape[1], output_shape[2]),
-				layer_num_param, '%s (%s, %s)' % (memory_format, memory_data_format, memory_param_format)))
-			print('-------------------------------------------------------------------------------------------------------')
-			total_param += layer_num_param
-			previous_layer_shape = [output_shape]
-
-		total_data_usage = sum(memory_data_list.values())
-		total_param_usage = sum(memory_param_list.values())
-		total_memory = dict()
-		total_memory['data'] = total_data_usage
-		total_memory['param'] = total_param_usage
-		total_memory_usage = sum(total_memory.values())
-		print('Total params: {}'.format(total_param))
-		print('Total memory usage: {}'.format(humanize.naturalsize(total_memory_usage, binary=True)))
-		print('=======================================================================================================')
-
-		# remove small percentage during display
-		remove_threshold = 0.01
-		for key, value in memory_data_list.items():
-			if value < remove_threshold * total_data_usage:
-				del memory_data_list[key]
-		for key, value in memory_param_list.items():
-			if value < remove_threshold * total_param_usage:
-				del memory_param_list[key]
-		for key, value in total_memory.items():
-			if value < remove_threshold * total_memory_usage:
-				del total_memory[key]
-
-		# plot pie chart for memory usage
-		threshold_display_percentage = 5
-		fig = plt.figure()
-		gs = gridspec.GridSpec(2, 2)
-		fig.suptitle('Network Memory Usage', fontsize=16)
-		ax = plt.subplot(gs[0, 0])
-		ax.set_title('Data Memory Usage: %s' % humanize.naturalsize(total_data_usage, binary=True), fontsize=10)
-		max_index = np.argmax(np.array(memory_data_list.values()))	# find the one to explode
-		explode_list = [0] * len(memory_data_list)
-		explode_list[max_index] = 0.1
-		text = plt.pie(memory_data_list.values(), labels=memory_data_list.keys(), autopct=autopct_generator(threshold_display_percentage), shadow=True, explode=explode_list)[1]
-		# fixOverLappingText(text)
-		plt.axis('equal')
-		ax = plt.subplot(gs[0, 1])
-		ax.set_title('Parameter Memory Usage: %s' % humanize.naturalsize(total_param_usage, binary=True), fontsize=10)
-		max_index = np.argmax(np.array(memory_param_list.values()))	# find the one to explode
-		explode_list = [0] * len(memory_param_list)
-		explode_list[max_index] = 0.1
-		text = plt.pie(memory_param_list.values(), labels=memory_param_list.keys(), autopct=autopct_generator(threshold_display_percentage), shadow=True, explode=explode_list)[1]
-		# fixOverLappingText(text)
-		plt.axis('equal')
-		ax = plt.subplot(gs[1, :])
-		ax.set_title('Total Memory Usage: %s' % humanize.naturalsize(total_memory_usage, binary=True), fontsize=10)
-		max_index = np.argmax(np.array(total_memory.values()))	# find the one to explode
-		explode_list = [0] * len(total_memory)
-		explode_list[max_index] = 0.1
-		text = plt.pie(total_memory.values(), labels=total_memory.keys(), autopct=autopct_generator(threshold_display_percentage), shadow=True, explode=explode_list)[1]
-		# fixOverLappingText(text)
-		plt.axis('equal')
-		plt.savefig(savepath)
-		plt.show()
-
-
 	def get_memory_usage_data_layer(self, layer_name):
 		'''
 		this function return memory usage for data given a specific layer
@@ -218,3 +137,114 @@ class SequentialNet(Net):
  			return num_pixel			# unsigned integer has 1 byte
 		else:
 			assert False, 'unknown error while calculating memory usage for data'
+
+	def summary(self, table_path=None, chart_path=None, display_threshold=5, remove_threshold=1):
+		'''
+		print all info about the network to terminal and local path
+
+		input parameter:
+			table_path: path to save network info for all layers including number of parameter, memory usage, output shape, layer name, layer type
+			
+			chart_path: path to save memory usage chart, which will display how much percentage of memory to take for each layer
+
+			display_threshold: due to limited space of pie chart, we may not want to display the percentage number for some layers which only take a
+							very small piece of pie. So, we suppress them by using threshold. For example, if the threshold is 5, percentage number
+							lower than 5% will not be displayed 
+
+			remove_threshold: we may also want to remove some layers totally from the pie chart, which takes up very little memory space and will overlap
+							in the pie chart and make it ugly. So, we remove them by using this threshold. For example, if the threshold is 1, percentage
+							lower than 1% will be removed 
+		'''
+		assert self._compiled, 'the network is not compiled'
+		assert table_path is None or is_path_exists_or_creatable(table_path), 'table path is not correct'
+		assert chart_path is None or is_path_exists_or_creatable(chart_path), 'chart path is not correct'
+		if table_path is None:
+			table_path = 'table.txt'
+		if chart_path is None:
+			chart_path = 'chart.png'
+
+		# print terminal network info to a table and file
+
+		file_handler = open(table_path, 'w')
+		print >> file_handler, 'Network Info Summary'
+		print >> file_handler, '======================================================================================================='
+		print >> file_handler, '{:<30}{:<25}{:<15}{:<20}'.format('Layer (type)', 'Output Shape', 'Param', 'Memory Usage(data, param)')
+		print >> file_handler, '-------------------------------------------------------------------------------------------------------'
+		total_param = 0
+		memory_data_list = dict()
+		memory_param_list = dict()
+		previous_layer_shape = None
+		for layer_name in self._blobs.keys():
+			layer = self._layers[layer_name]
+			output_shape = self._blobs[layer_name]['data'].shape
+			layer_num_param = layer.get_num_param(previous_layer_shape)
+			memory_data = self.get_memory_usage_data_layer(layer_name)
+			memory_param = layer.get_memory_usage_param(previous_layer_shape)
+			memory_data_list[layer_name] = memory_data
+			memory_param_list[layer_name] = memory_param
+			memory = memory_data + memory_param
+			memory_data_format = humanize.naturalsize(memory_data, binary=True)
+			memory_param_format = humanize.naturalsize(memory_param, binary=True)
+			memory_format = humanize.naturalsize(memory, binary=True)
+			print >> file_handler, '{:<30}{:<25}{:<15}{:<20}'.format('%s (%s)' % (layer_name, layer.type), '(%d, %d, %d, %d)' % (self._batch_size, output_shape[0], output_shape[1], output_shape[2]),
+				layer_num_param, '%s (%s, %s)' % (memory_format, memory_data_format, memory_param_format))
+			print >> file_handler, '-------------------------------------------------------------------------------------------------------'
+			total_param += layer_num_param
+			previous_layer_shape = [output_shape]
+
+		total_data_usage = sum(memory_data_list.values())
+		total_param_usage = sum(memory_param_list.values())
+		total_memory = dict()
+		total_memory['data'] = total_data_usage
+		total_memory['param'] = total_param_usage
+		total_memory_usage = sum(total_memory.values())
+		print >> file_handler, 'Total params: {}'.format(total_param)
+		print >> file_handler, 'Total memory usage: {}'.format(humanize.naturalsize(total_memory_usage, binary=True))
+		print >> file_handler, '======================================================================================================='
+		file_handler.close()
+
+		# remove small percentage during display
+		for key, value in memory_data_list.items():
+			if value < remove_threshold * total_data_usage / 100.:
+				del memory_data_list[key]
+		for key, value in memory_param_list.items():
+			if value < remove_threshold * total_param_usage / 100.:
+				del memory_param_list[key]
+		for key, value in total_memory.items():
+			if value < remove_threshold * total_memory_usage / 100.:
+				del total_memory[key]
+
+		# plot pie chart for memory usage
+		fig = plt.figure()
+		gs = gridspec.GridSpec(2, 2)
+		fig.suptitle('Network Memory Usage', fontsize=16)
+		ax = plt.subplot(gs[0, 0])
+		ax.set_title('Data Memory Usage: %s' % humanize.naturalsize(total_data_usage, binary=True), fontsize=10)
+		max_index = np.argmax(np.array(memory_data_list.values()))	# find the one to explode
+		explode_list = [0] * len(memory_data_list)
+		explode_list[max_index] = 0.1
+		text = plt.pie(memory_data_list.values(), labels=memory_data_list.keys(), autopct=autopct_generator(display_threshold), shadow=True, explode=explode_list)[1]
+		# fixOverLappingText(text)
+		plt.axis('equal')
+		ax = plt.subplot(gs[0, 1])
+		ax.set_title('Parameter Memory Usage: %s' % humanize.naturalsize(total_param_usage, binary=True), fontsize=10)
+		max_index = np.argmax(np.array(memory_param_list.values()))	# find the one to explode
+		explode_list = [0] * len(memory_param_list)
+		explode_list[max_index] = 0.1
+		text = plt.pie(memory_param_list.values(), labels=memory_param_list.keys(), autopct=autopct_generator(display_threshold), shadow=True, explode=explode_list)[1]
+		# fixOverLappingText(text)
+		plt.axis('equal')
+		ax = plt.subplot(gs[1, :])
+		ax.set_title('Total Memory Usage: %s' % humanize.naturalsize(total_memory_usage, binary=True), fontsize=10)
+		max_index = np.argmax(np.array(total_memory.values()))	# find the one to explode
+		explode_list = [0] * len(total_memory)
+		explode_list[max_index] = 0.1
+		text = plt.pie(total_memory.values(), labels=total_memory.keys(), autopct=autopct_generator(display_threshold), shadow=True, explode=explode_list)[1]
+		# fixOverLappingText(text)
+		plt.axis('equal')
+		plt.savefig(chart_path)
+		# plt.show()
+
+		print ' '
+		print 'Network info table is saved to %s' % os.path.abspath(table_path)
+		print 'Network memory usage chart is saved to %s' % os.path.abspath(chart_path) 
