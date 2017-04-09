b955b8d063779d2e509458862a3a7f5259594861
xinshuoweng <xinshuo.weng@gmail.com>
2017-04-08 21:35:27 -0400

    update

--- python/conversions.py
+++ python/conversions.py
@@ -31,6 +31,37 @@ def string2onehot(string):
 		one_hot_vec = np.vstack((one_hot_vec, [character2onehot(ord(character))]))
 	return one_hot_vec
 
+def onehot2ord(onehot):
+	'''
+	convert one hot vector to a ord integer number
+	'''
+	assert isinstance(onehot, np.ndarray) and onehot.ndim == 1, 'input should be 1-d numpy array'
+	assert sum(onehot) == 1 and np.count_nonzero(onehot) == 1, 'input numpy array is not one hot vector'
+	return np.argmax(onehot)
+
+def onehot2character(onehot):
+	'''
+	convert one hot vector to a character
+	'''
+	return chr(onehot2ord(onehot))
+
+def onehot2string(onehot):
+	'''
+	convert a set of one hot vector to a string
+	'''
+	if isinstance(onehot, np.ndarray):
+		onehot.ndim == 2, 'input should be 2-d numpy array'
+		onehot = list(onehot)
+	elif isinstance(onehot, list):
+		assert CHECK_EQ_LIST([tmp.ndim for tmp in onehot]), 'input list of one hot vector should have same length'
+	else:
+		assert False, 'unknown error'
+
+	assert all(sum(onehot_tmp) == 1 and np.count_nonzero(onehot_tmp) == 1 for onehot_tmp in onehot), 'input numpy array is not a set of one hot vector'
+	ord_list = [onehot2ord(onehot_tmp) for onehot_tmp in onehot]
+	return ord2string(ord_list)
+
+
 def string2ord(string):
 		'''
 	convert a string to a list of ASCII character
@@ -43,11 +74,11 @@ def string2ord(string):
 	return ord_list
 
 def ord2string(ord_list):
-		'''
+	'''
 	convert a list of ASCII character to a string
 	'''
-	assert islist(ord_list) and len(ord_list) > 0, 'input should be a list of ord with length larger than 0'
-	assert all(isinteger(type(tmp)) for tmp in ord_list), 'all elements in the list of ord should be integer'
+	assert isinstance(ord_list, list) and len(ord_list) > 0, 'input should be a list of ord with length larger than 0'
+	assert all(isinteger(tmp) for tmp in ord_list), 'all elements in the list of ord should be integer'
 	
 	L = ''
 	for o in ord_list:
