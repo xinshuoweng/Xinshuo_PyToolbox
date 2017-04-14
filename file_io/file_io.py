4115f35f7e1f552c93b86a43540cfe32ee884051
xinshuoweng <xinshuo.weng@gmail.com>
2017-04-14 13:44:25 -0400

    add loading list from folder

--- file_io/file_io.py
+++ file_io/file_io.py
@@ -3,6 +3,7 @@
 
 # this file contains a set of function for manipulating file io in python
 import os, sys
+import glob
 
 import __init__paths__
 from check import is_path_exists, isstring, is_path_exists_or_creatable, isfile, isfolder
@@ -25,14 +26,14 @@ def file_abspath():
     '''
     return os.path.dirname(os.path.abspath(__file__))
 
-def load_list_from_file(pathname):
+def load_list_from_file(file_path):
     '''
     this function reads list from a txt file
     '''
-    assert is_path_exists(pathname), 'input path does not exist'
-    _, _, extension = fileparts(pathname)
+    assert is_path_exists(file_path), 'input path does not exist'
+    _, _, extension = fileparts(file_path)
     assert extension == '.txt', 'File doesn''t have valid extension.'
-    file = open(pathname, 'r')
+    file = open(file_path, 'r')
     assert file != -1, 'datalist not found'
 
     fulllist = file.read().splitlines()
@@ -41,6 +42,22 @@ def load_list_from_file(pathname):
 
     return fulllist, num_elem
 
+
+def load_list_from_folder(folder_path, ext_filter=None):
+    '''
+    load a list of files from a system folder
+    '''
+    assert isfolder(folder_path) and is_path_exists(folder_path), 'input folder path is not correct'
+    if ext_filter is not None:
+        assert isstring(ext_filter), 'extension filder is not correct'
+        fulllist = glob.glob(os.path.join(folder_path, '*%s' % ext_filter))
+    else:
+        fulllist = glob.glob(folder_path)
+    num_elem = len(fulllist)
+    
+    return fulllist, num_elem
+
+
 def mkdir_if_missing(pathname):
     if isfile(pathname):
         pathname, _, _ = fileparts(pathname)
@@ -48,8 +65,9 @@ def mkdir_if_missing(pathname):
     if not is_path_exists(pathname):
         os.mkdir(pathname)
 
+
 def generate_list_from_folder(save_path, src_path, ext_filter=None):
-    assert isfolder(src_path) and if_path_exists(src_path), 'source folder not found or incorrect'
+    assert isfolder(src_path) and is_path_exists(src_path), 'source folder not found or incorrect'
     if not isfile(save_path):
         assert isfolder(save_path), 'save path is not correct'
         save_path = os.path.join(save_path, 'datalist.txt')
