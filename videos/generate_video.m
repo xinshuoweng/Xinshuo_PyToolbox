13bb73c40549dbcec3072cbba2dbbb3ecd8a8a8b
xinshuoweng <xinshuo.weng@gmail.com>
2017-04-25 13:40:22 -0400

    update video process

--- videos/generate_video.m
+++ videos/generate_video.m
@@ -1,24 +1,22 @@
 
 % TODO: CHECK
 % script for generating video 
-clear;
-close all;
-clc;
+function [num_images] = generate_video_from_folder(img_src, save_path, videoname)
 
 % seqname = sprintf('set%02d/V0%02d.seq', i, k);
-videoname = 'testing/video/';
 % imagefolder = sprintf('images/SET%02d_V0%02d', i, k);
-sourcefolder = 'testing/images/';
+% sourcefolder = img_src;
 
-imagenames = dir(fullfile(sourcefolder, 'output3*.jpg'));
-imagenames = {imagenames.name}';
+% imagenames = dir(fullfile(sourcefolder, 'output3*.jpg'));
+% imagenames = {imagenames.name}';
+imagelist = load_list_from_folder(img_src);
 % Is = seqIo(seqname, 'frImgs', [], 'sDir', sourcefolder);
-video = VideoWriter(fullfile(videoname, 'output3.avi'), 'Uncompressed AVI');
+video = VideoWriter(fullfile(save_path, sprintf('%s.avi', videoname)), 'Uncompressed AVI');
 video.FrameRate = 30;
 open(video);
 
-for i = 1:length(imagenames)
-    img = imread(fullfile(sourcefolder, imagenames{i}));
+for i = 1:length(imagelist)
+    img = imread(imagelist{i});
     writeVideo(video, img);
 end
 
