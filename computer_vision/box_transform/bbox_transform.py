c6b6771b7a703994fba8071c9587fe4c415c00ac
xinshuoweng <xinshuo.weng@gmail.com>
2017-04-27 22:57:53 -0400

    fix bug

--- computer_vision/box_transform/bbox_transform.py
+++ computer_vision/box_transform/bbox_transform.py
@@ -9,11 +9,61 @@ import __init__paths__
 import numpy as np
 import math
 import time
-import math_function as mf
-from image_processing import imagecoor2cartesian, cartesian2imagecoor
+# import math_function as mf
+# from image_processing import imagecoor2cartesian, cartesian2imagecoor
 from numpy.testing import assert_almost_equal
 from math import radians as rad
 from check import isnparray
+
+
+# convert the point coordinate in the image, y axis is inverse
+def convert_pts(pts):
+    return np.array([pts[0], -pts[1]], dtype=float)
+
+def convert_pts_back2image(pts):
+    return np.array([pts[0], -pts[1]], dtype=float)
+
+# slope is the angle
+def get_line(pts, slope):
+    if slope == 90 or -90:
+        slope = slope + 0.00001
+    slope = math.tan(math.radians(slope))
+    # print('slope is ' + str(slope))
+    dividor = slope * pts[0] - pts[1]
+    if dividor == 0:
+        dividor += 0.00001
+    b = 1.0 / dividor
+    a = -b * slope
+    # assert math.fabs(pts[0]*a + pts[1]*b + 1) < 0.0000001, 'Point is not on the line'
+    return np.array([a, b, 1], dtype=float)
+
+def get_slope(pts1, pts2):
+    slope = (pts1[1] - pts2[1]) / (pts1[0] - pts2[0])
+    # print(slope)
+    slope = np.arctan(slope)
+    # print(slope)
+    slope = math.degrees(slope)
+    # print(slope)
+    return slope
+
+
+def get_intersection(line1, line2):
+    a1 = line1[0]
+    b1 = line1[1]
+    a2 = line2[0]
+    b2 = line2[1]
+    dividor = a2 * b1 - a1 * b2
+    if dividor == 0:
+        dividor += 0.00001
+    y = (a1 - a2) / dividor
+    if a1 == 0:
+        a1 += 0.00001
+    x = (-1.0 - b1 * y) / a1
+    # assert math.fabs(x*line1[0] + y*line1[1] + 1) < 0.0000001, 'Intersection point is not on the line'
+    # assert math.fabs(x*line2[0] + y*line2[1] + 1) < 0.0000001, 'Intersection point is not on the line'
+    return np.array([x, y], dtype=float)
+
+
 
 def bbox_transform(ex_rois, gt_rois):
     ex_widths = ex_rois[:, 2] - ex_rois[:, 0] + 1.0
@@ -139,10 +189,10 @@ def bbox_rotatedtight2rotatedloose(bbox_in, angle_in_degree, debug=True):
 
     pts_tl = np.array([bbox_in[0], bbox_in[1]])
     pts_br = np.array([bbox_in[2], bbox_in[3]])
-    line1 = mf.get_line(imagecoor2cartesian(pts_tl, debug=debug), angle_in_degree + 90.00, debug=debug)
-    line2 = mf.get_line(imagecoor2cartesian(pts_br, debug=debug), angle_in_degree, debug=debug)
-    pts_bl = cartesian2imagecoor(mf.get_intersection(line1, line2, debug=debug), debug=debug)
-    pts_tr = cartesian2imagecoor(mf.get_intersection(mf.get_line(imagecoor2cartesian(pts_tl, debug=debug), angle_in_degree, debug=debug), mf.get_line(imagecoor2cartesian(pts_br, debug=debug), angle_in_degree + 90.00, debug=debug), debug=debug), debug=debug)
+    line1 = get_line(convert_pts(pts_tl), angle_in_degree + 90.00)
+    line2 = get_line(convert_pts(pts_br), angle_in_degree)
+    pts_bl = convert_pts_back2image(get_intersection(line1, line2))
+    pts_tr = convert_pts_back2image(get_intersection(get_line(convert_pts(pts_tl), angle_in_degree), get_line(convert_pts(pts_br), angle_in_degree + 90.00)))
     # assert_almost_equal(np.dot(pts_bl - pts_br, pts_bl - pts_tl), 0, err_msg='The intersection points are wrong')
     # assert_almost_equal(np.dot(pts_tr - pts_br, pts_tr - pts_tl), 0, err_msg='The intersection points are wrong')
 
