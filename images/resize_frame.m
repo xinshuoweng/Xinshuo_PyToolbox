bb873cb95061d301ab969d0a2408220fb273586b
xinshuoweng <xinshuo.weng@gmail.com>
2017-04-30 10:37:21 -0400

    update

--- images/resize_frame.m
+++ images/resize_frame.m
@@ -1,56 +1,24 @@
-% this script is for resizing the original raw frame from towncenter to different size for evaluation
+% Author: Xinshuo Weng
+% email: xinshuo.weng@gmail.com
 
-close all; clear; clc;
-% TODO: CHECK
-%% inputs to function
-data_base = '../7/';
-frame_list = dir(fullfile([data_base, '*.png']));
-fnum      = length(frame_list);        % 54, 105, 111, 214, 256, 300
-save_dir = '../7_crop640_480';
-% resize_size = [180, 320];
+function num_images = resize_images(src_path, resize_size, debug_mode)
+	if debug_mode
+		assert(ischar(src_path), 'source path is not correct');
+		assert(isvector(resize_size) && numel(resize_size) == 2, 'resize size is not correct');
+	end
 
-if ~exist(save_dir, 'dir')
-	mkdir(save_dir);
+	[image_list, num_images] = load_list_from_folder(src_path, 'jpg');
+	save_dir = fullfile(src_path, '../resized');
+	mkdir_if_missing(save_dir);
+
+	for i = 1:num_images
+		fprintf('processing %d/%d', i, num_images);
+		image_path_temp = image_list{i};
+		image_temp = imread(image_path_temp);
+		[~, filename, ~] = fileparts(image_temp);
+		image_temp = imresize(image_temp, resize_size);
+		save_path = fullfile(save_dir, sprintf('%s.jpg', filename));
+		imwrite(image_temp, save_path);
+	end
+
 end
-
-% number_resize = size(resize_size, 1);
-% for j = 1:number_resize
-	% resize_temp = resize_size(j, :);
-
-for i = 1:fnum
-	image_path_temp = fullfile([data_base, frame_list(i).name]);
-	image_temp = imread(image_path_temp);
-	image_temp = image_temp(380:900-1, 600:1320-1, :);
-
-	name = frame_list(i).name;
-	index2 = strfind(name, '.png');
-	id = str2double(name(index2-5:index2-1));
-	% fprintf('%d', id);
-	% pause(1);
-	save_dir_temp = fullfile([save_dir, sprintf('/frame%05d.png', id)]);
-	imwrite(image_temp, save_dir_temp);
-	fprintf('loading image %d/%d\n', i, fnum);
-end
-
-% end
-% fname_in  = fullfile(fpath_in,  sprintf('frame%06d.jpg', fnum));
-% fname_out = fullfile(fpath_out, sprintf('frame%06d.jpg', fnum));
-
-% crop.w = 47;
-% crop.h = 87;
-
-
-% %% main function: crop_location
-% %- draw rectangle
-% pos.x = 224; pos.y = 74;
-% pos.rec = [pos.x, pos.y, crop.w, crop.h];
-% img = imread(fname_in);
-% figure; imshow(img); hold on;
-% rectangle('Position', pos.rec); 
-
-% %- crop
-% img_crop = img(pos.y:pos.y+crop.h, pos.x:pos.x+crop.w, :);
-% %figure; imshow(img_crop);
-
-% %- save
-% imwrite(img_crop, fname_out);
