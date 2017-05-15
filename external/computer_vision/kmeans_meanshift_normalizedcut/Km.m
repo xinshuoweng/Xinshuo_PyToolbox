045f32566e0b9da306ff8e469dd74b28dc35ec27
xinshuoweng <xinshuo.weng@gmail.com>
2017-05-15 18:40:34 -0400

    change structure

---
+++ external/computer_vision/kmeans_meanshift_normalizedcut/Km.m
@@ -0,0 +1,38 @@
+function Ikm = Km(I,K) 
+
+%% K-means Segmentation (option: K (Number of Clusters))
+I = im2double(I);
+F = reshape(I,size(I,1)*size(I,2),3);                 % Color Features
+%% K-means
+CENTS = F( ceil(rand(K,1)*size(F,1)) ,:);             % Cluster Centers
+DAL   = zeros(size(F,1),K+2);                         % Distances and Labels
+KMI   = 10;                                           % K-means Iteration
+for n = 1:KMI
+   for i = 1:size(F,1)
+      for j = 1:K  
+        DAL(i,j) = norm(F(i,:) - CENTS(j,:));      
+      end
+      [Distance CN] = min(DAL(i,1:K));                % 1:K are Distance from Cluster Centers 1:K 
+      DAL(i,K+1) = CN;                                % K+1 is Cluster Label
+      DAL(i,K+2) = Distance;                          % K+2 is Minimum Distance
+   end
+   for i = 1:K
+      A = (DAL(:,K+1) == i);                          % Cluster K Points
+      CENTS(i,:) = mean(F(A,:));                      % New Cluster Centers
+      if sum(isnan(CENTS(:))) ~= 0                    % If CENTS(i,:) Is Nan Then Replace It With Random Point
+         NC = find(isnan(CENTS(:,1)) == 1);           % Find Nan Centers
+         for Ind = 1:size(NC,1)
+         CENTS(NC(Ind),:) = F(randi(size(F,1)),:);
+         end
+      end
+   end
+end
+
+X = zeros(size(F));
+for i = 1:K
+idx = find(DAL(:,K+1) == i);
+X(idx,:) = repmat(CENTS(i,:),size(idx,1),1); 
+end
+Ikm = reshape(X,size(I,1),size(I,2),3);
+
+end
