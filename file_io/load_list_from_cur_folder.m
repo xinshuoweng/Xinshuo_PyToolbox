271b9d1c2ecbf0a29aaa814135bfe377c965df3f
xinshuoweng <xinshuo.weng@gmail.com>
2017-07-08 00:16:51 -0400

    add clean tool and parallel tool

--- file_io/load_list_from_cur_folder.m
+++ file_io/load_list_from_cur_folder.m
@@ -10,22 +10,20 @@ function full_image_list = load_list_from_cur_folder(folder_path, ext_filter, de
     if debug_mode
 		assert(ischar(folder_path), 'Input path is not valid for obtaining list');
 	end
-	ext_filter = check_extension(ext_filter, debug_mode);
-
-	% fprintf('Warning! Only jpeg jpg png bmp extension are supported!\n');
 	image_list = dir(folder_path);
 	if debug_mode
 		assert(~isempty(folder_path), 'The input path not found while obtaining the image list!');
 	end
 	image_id_list = {image_list(:).name};
-	keep_logical_jpg = cellfun(@(x) ~isempty(x), strfind(image_id_list, ext_filter));
 
-	% keep_logical_jpeg = cellfun(@(x) ~isempty(x), strfind(image_id_list, 'jpeg'));
-	% keep_logical_png = cellfun(@(x) ~isempty(x), strfind(image_id_list, 'png'));
-	% keep_logical_bmp = cellfun(@(x) ~isempty(x), strfind(image_id_list, 'bmp'));
-	% keep_logical = keep_logical_bmp | keep_logical_png | keep_logical_jpeg | keep_logical_jpg;
-	
-	keep_logical = keep_logical_jpg;
+	keep_logical = zeros(1, length(image_id_list));
+	for ext_index = 1:length(ext_filter)
+		ext_filter_tmp = ext_filter(ext_index);
+		ext_filter_tmp = check_extension(ext_filter_tmp, debug_mode);
+		keep_logical_tmp = cellfun(@(x) ~isempty(x), strfind(image_id_list, ext_filter_tmp));
+		keep_logical = keep_logical | keep_logical_tmp;
+	end
+
 	keep = find(keep_logical);		% find the index of the cell which is image
 	image_list = {image_list(keep).name};
 	full_image_list = cellfun(@(x) fullfile(folder_path, x), image_list, 'UniformOutput', false);	% concatenate with parent folder path
--- file_io/load_list_from_folder.m
+++ file_io/load_list_from_folder.m
@@ -8,15 +8,20 @@ function [full_image_list, num_image] = load_list_from_folder(folder_path, ext_f
         debug_mode = true;
     end
 
+    depth = intmax;
     if debug_mode
     	assert(ischar(folder_path), 'Input path is not valid for obtaining list');
     end
-    ext_filter = check_extension(ext_filter, debug_mode);
 
-	subfolder_list = get_subfolder_list(folder_path, debug_mode);
-    imagelist_subfolder = remove_empty_cell(cellfun(@(x) load_list_from_cur_folder(x, ext_filter, debug_mode), subfolder_list, 'UniformOutput', false), debug_mode);	
+	subfolder_list = get_subfolder_list(folder_path, depth, debug_mode);
+    imagelist_subfolder = remove_empty_cell(cellfun(@(x) load_list_from_cur_folder(x, ext_filter, debug_mode), subfolder_list, 'UniformOutput', false), debug_mode);
     imagelist_curfolder = load_list_from_cur_folder(folder_path, ext_filter, debug_mode);
-    full_image_list = [imagelist_curfolder, imagelist_subfolder];	
+
+    % merge all sub-imagelist
+    full_image_list = imagelist_curfolder;
+    for cell_index = 1:length(imagelist_subfolder)
+        full_image_list = [full_image_list, imagelist_subfolder{cell_index}];
+    end
     num_image = length(full_image_list);
     
     % optional for saving
