6d76154060e59dffa51ae78eca497f1701deadde
xinshuoweng <xinshuo.weng@gmail.com>
2017-09-14 21:54:56 -0400

    update

---
+++ python/__init__.py
@@ -0,0 +1,14 @@
+# Author: Xinshuo Weng
+# email: xinshuo.weng@gmail.com
+
+# this file is for the adding the path for python library
+# import os, sys
+
+# def add_path(path):
+#     if path not in sys.path:
+#         sys.path.insert(0, path)
+
+# this_dir = os.path.dirname(os.path.abspath(__file__))
+
+# file_path = os.path.join(this_dir, '../file_io')
+# add_path(file_path)
