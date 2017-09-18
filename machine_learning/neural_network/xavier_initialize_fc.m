c14a53a22b0f147280232761a50f7f179419b026
xinshuo <xinshuo.weng@gmail.com>
2017-09-17 23:48:57 -0400

    update

--- machine_learning/neural_network/xavier_initialize_fc.m
+++ machine_learning/neural_network/xavier_initialize_fc.m
@@ -3,19 +3,23 @@
 
 
 % given a list of numbers for fully connected layers, initialize the weight with Xavier initialization
-function [W, b] = xavier_initialize_fc(layers, debug_mode)
-	if debug_mode
-		assert(isvector(layers), 'the input fully connected layers should be a list');
-		assert(all(layers > 0), 'all fully connected layers should be larger than 0');
+function [W, b] = xavier_initialize_fc(net, debug_mode)
+	if nargin < 2
+		debug_mode = true;
 	end
 
-	number_layer = length(layers);
+	if debug_mode
+		assert(isvector(net), 'the input fully connected layers should be a list');
+		assert(all(net > 0), 'all fully connected layers should be larger than 0');
+	end
+
+	number_layer = length(net);
 	W = cell(1, number_layer-1);
 	b = cell(1, number_layer-1);
 
 	% compute N_in and N_out for W at each layer
 	for i = 1:number_layer-1
-		W{i} = normrnd(0, 2/(layers(i) + layers(i+1)), [layers(i+1), layers(i)]);
-		b{i} = normrnd(0, 2/(layers(i+1) + 1), [layers(i+1), 1]);
+		W{i} = normrnd(0, 2/(net(i) + net(i+1)), [net(i+1), net(i)]);
+		b{i} = normrnd(0, 2/(net(i+1) + 1), [net(i+1), 1]);
 	end
 end
