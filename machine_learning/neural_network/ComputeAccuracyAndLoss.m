7701724e7d2a61e318da787ef21ffb19f6357ed9
xinshuo <xinshuo.weng@gmail.com>
2017-09-17 23:26:02 -0400

    update

--- machine_learning/neural_network/ComputeAccuracyAndLoss.m
+++ machine_learning/neural_network/ComputeAccuracyAndLoss.m
@@ -6,14 +6,14 @@ function [accuracy, loss] = ComputeAccuracyAndLoss(W, b, data, labels)
 
 % accuracy is the classification rate
 
-number_examples = size(data, 1);
-outputs = Classify(W, b, data);		% D x C
-temp1 = outputs .* labels;
-temp2 = sum(temp1, 2);
-loss_total = -log(temp2);
-loss = sum(loss_total)/number_examples;
-[~, ex_id] = max(outputs, [], 2);
-[~, gt_id] = max(labels, [], 2);
-accuracy = (sum(ex_id - gt_id == 0))/size(labels, 1);
+	number_examples = size(data, 1);
+	outputs = Classify(W, b, data);		% D x C
+	temp1 = outputs .* labels;
+	temp2 = sum(temp1, 2);
+	loss_total = -log(temp2);
+	loss = sum(loss_total)/number_examples;
+	[~, ex_id] = max(outputs, [], 2);
+	[~, gt_id] = max(labels, [], 2);
+	accuracy = (sum(ex_id - gt_id == 0))/size(labels, 1);
 
 end
