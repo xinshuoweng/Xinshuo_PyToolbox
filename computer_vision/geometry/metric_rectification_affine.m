232b364d53962ac48d51a787ba90aff00a9b42fb
xinshuoweng <xinshuo.weng@gmail.com>
2017-10-03 18:41:29 -0400

    update

--- computer_vision/geometry/metric_rectification_affine.m
+++ computer_vision/geometry/metric_rectification_affine.m
@@ -33,6 +33,7 @@ function [rectified_img, H] = metric_rectification_affine(img, line_pairs, debug
 	right_side = [m; n];
 	s = params \ right_side;
 	conic_dual = [s(1), s(2), 0; s(2), 1, 0; 0, 0, 0];
+	conic_dual
 
 	% check orthogonality in dual space with affine distortion
 	if debug_mode
@@ -42,38 +43,44 @@ function [rectified_img, H] = metric_rectification_affine(img, line_pairs, debug
 	end
 
 	% check if the computed S is positive definite
-	if debug_mode
-		eigen = eig(conic_dual);
-		assert(all(eigen >= 0), sprintf('the computed K * K'' is not positive definite, the eigen values are %.3f, %.3f\n', eigen(1), eigen(2)));
-	end
+	% if debug_mode
+	% 	eigen = eig(conic_dual);
+	% 	assert(all(eigen >= 0), sprintf('the computed K * K'' is not positive definite, the eigen values are %.3f, %.3f\n', eigen(1), eigen(2)));
+	% end
 
-	K = chol(conic_dual(1:2, 1:2), 'lower');														% 2 x 2
-	
+	[L, D] = ldl(conic_dual)
+	% D(1) = -D(1);
+	% L*D*L'
+	% conic_dual
+	K = L * sqrt(sqrt(D^2));
+
+
+	% K = chol(conic_dual(1:2, 1:2), 'lower')														% 2 x 2
+
 	% check the decomposition such that K*K' = S
-	if debug_mode
-		residual = K * K' - conic_dual(1:2, 1:2);
-		assert(norm(residual) < 1e-4, 'the Cholesky decomposition is not correct');
-	end
+	% if debug_mode
+	% 	residual = K * K' - conic_dual(1:2, 1:2);
+	% 	assert(norm(residual) < 1e-4, 'the Cholesky decomposition is not correct');
+	% end
 	
 	K = [K(1, 1), K(1, 2), 0; K(2, 1), K(2, 2), 0; 0, 0, 1];					% 3 X 3
 	H = inv(K);																	% 3 x 3 matrix, this homography remove the affine distortion
 	
-	if debug_mode
-		conic = [1, 0, 0; 0, 1, 0; 0, 0, 0];
+	% if debug_mode
+	% 	conic = [1, 0, 0; 0, 1, 0; 0, 0, 0];
 
-		% check the transformation between conic and conic in the dual space
-		residual = K * conic * K' - conic_dual;
-		assert(norm(residual) < 1e-4, 'the affine rectification homography is wrong');
+	% 	% % check the transformation between conic and conic in the dual space
+	% 	% residual = K * conic * K' - conic_dual;
+	% 	% assert(norm(residual) < 1e-4, 'the affine rectification homography is wrong');
 		
-		% check the orthogonality in the image after affine distortion is removed
-		line1_ori = K' * line_pairs(1, :)';			% 3 x 1
-		line2_ori = K' * line_pairs(2, :)';
-		line3_ori = K' * line_pairs(3, :)';
-		line4_ori = K' * line_pairs(4, :)';
-		residual1 = line1_ori' * conic * line2_ori;
-		residual2 = line3_ori' * conic * line4_ori;
-		assert(residual1 < 1e-4 && residual2 < 1e-4, 'two pairs of lines are not orthogonal in the original space');
-	end
+	% 	% check the orthogonality in the image after affine distortion is removed
+	% 	line1_ori = K' * line_pairs(1, :)';			% 3 x 1
+	% 	line2_ori = K' * line_pairs(2, :)';
+	% 	line3_ori = K' * line_pairs(3, :)';
+	% 	line4_ori = K' * line_pairs(4, :)';
+	% 	residual1 = line1_ori' * conic * line2_ori;
+	% 	residual2 = line3_ori' * conic * line4_ori;
+	% 	assert(residual1 < 1e-4 && residual2 < 1e-4, 'two pairs of lines are not orthogonal in the original space');
+	% end
 	rectified_img = applyH(img, H);
-	imshow(rectified_img)
 end
--- visualization/visualize_segment.m
+++ visualization/visualize_segment.m
@@ -2,17 +2,19 @@
 % email: xinshuo.weng@gmail.com
 
 % visualize a heatmap on top of an image
-function visualize_segment(pts_array, fig, debug_mode)
+function visualize_segment(pts_array, fig, color_index, debug_mode)
     if nargin < 3
         debug_mode = true;
     end
+
+    color_set = ['r', 'g', 'b', 'y', 'm', 'c', 'w', 'k'];
 
     if debug_mode
         assert(all(size(pts_array) == [2, 2]), 'the input point array is not correct');
     end
 
     figure(fig);
-    plot(pts_array(:, 1), pts_array(:, 2), '-rx');
+    plot(pts_array(:, 1), pts_array(:, 2), 'Marker', 'x', 'Color', color_set(color_index));
 
     % max_value = max(max(heatmap));
     % im_to_disp = (img + mat2im(heatmap, jet(100), [0 max_value])) / 2;  % apply heatmap on the original image
