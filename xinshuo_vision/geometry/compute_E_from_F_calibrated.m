ea6cbccc3b81c033be51f16fb50819da850b3529
xinshuoweng <xinshuo.weng@gmail.com>
2017-10-26 11:03:54 -0400

    i[date

--- xinshuo_vision/geometry/compute_E_from_F_calibrated.m
+++ xinshuo_vision/geometry/compute_E_from_F_calibrated.m
@@ -9,12 +9,28 @@ function E = compute_E_from_F_calibrated(F, K1, K2, debug_mode);
 		debug_mode = true;
 	end
 
+<<<<<<< HEAD
+	epsilon = 1e-5;
+=======
+>>>>>>> 4ae903eec3bec55dc48915cc61cc47602218f971
 	if debug_mode
 		assert(all(size(F) == [3, 3]), 'the input fudamental matrix does not have a good shape');
 		assert(all(size(K1) == [3, 3]), 'the input intrinsic matrix1 does not have a good shape');
 		assert(all(size(K2) == [3, 3]), 'the input intrinsic matrix2 does not have a good shape');
 		assert(isUpper(K1) && isUpper(K2), 'the input intrinsic matrix is not upper triangular');
+<<<<<<< HEAD
+		assert(det(F) < epsilon, 'the determinant of fundamental matrix is not close to 0')
 	end
 
 	E = K2' * F * K1;
+
+	det(E)
+	2 * E * E' * E - trace(E * E') * E
+	svd(E)
+	pause;
+=======
+	end
+
+	E = K2' * F * K1;
+>>>>>>> 4ae903eec3bec55dc48915cc61cc47602218f971
 end
--- xinshuo_vision/geometry/compute_R_t_from_E.m
+++ xinshuo_vision/geometry/compute_R_t_from_E.m
@@ -7,8 +7,15 @@ function [R, t] = compute_R_t_from_E(E, debug_mode);
 		debug_mode = true;
 	end
 
+<<<<<<< HEAD
+	epsilon = 1e-5;
 	if debug_mode
 		assert(all(size(E) == [3, 3]), 'the input essential matrix does not have a good shape');
+		assert(det(E) < epsilon, 'the determinant of essential matrix is not close to 0');
+=======
+	if debug_mode
+		assert(all(size(E) == [3, 3]), 'the input essential matrix does not have a good shape');
+>>>>>>> 4ae903eec3bec55dc48915cc61cc47602218f971
 	end
 
 	[U S V] = svd(E);
@@ -16,4 +23,9 @@ function [R, t] = compute_R_t_from_E(E, debug_mode);
 
 	t_skew = U * W * S * U';
 	R = U * W' * V'; 
+<<<<<<< HEAD
+
+	t_skew
+=======
+>>>>>>> 4ae903eec3bec55dc48915cc61cc47602218f971
 end
