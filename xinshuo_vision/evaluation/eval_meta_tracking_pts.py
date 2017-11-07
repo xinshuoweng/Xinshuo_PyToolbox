42e07a04acb5096742c95c865432bc8c52f96c59
xinshuoweng <xinshuo.weng@gmail.com>
2017-11-06 21:31:52 -0500

    fix bugs in eval meta tracking

--- xinshuo_vision/evaluation/eval_meta_tracking_pts.py
+++ xinshuo_vision/evaluation/eval_meta_tracking_pts.py
@@ -1,14 +1,12 @@
-import os, time
+# Author: Xinshuo
+# Email: xinshuow@andrew.cmu.edu
+import os, time, torch, json
 import numpy as np
-import torch
-import json
-from collections import OrderedDict
-from scipy import interpolate
-from mpl_toolkits.mplot3d import Axes3D
 
-from xinshuo_python import isnonnegativeinteger, is2dptsarray, isstring, is2dptsarray_occlusion
+from xinshuo_python import isnonnegativeinteger, is2dptsarray, isstring, is2dptsarray_occlusion, is_path_exists_or_creatable, is_path_exists
 from xinshuo_miscellaneous import print_log, print_np_shape
 from xinshuo_math import pts_euclidean
+
 # zero-indexed
 
 class eval_meta_tracking_pts():
@@ -27,8 +25,8 @@ class eval_meta_tracking_pts():
 		self.image_prev_path = dict()
 		self.image_next_path = dict()
 
-		self.pts_valid_index = dict()
-		self.key_list = []
+		self.pts_valid_index = dict()			# given key, provide a list of valid index
+		self.key_list = []						# current available keys
 		self.index_dict = dict()				# given key, provide index
 		self.length = 0
 
@@ -78,20 +76,32 @@ class eval_meta_tracking_pts():
 				'pts_anno':     	self.pts_anno,
 				'image_prev_path':  self.image_prev_path,
 				'image_next_path':  self.image_next_path,
+
 				'length':			self.length,
 				'pts_valid_index':	self.pts_valid_index,
+				'index_dict':		self.index_dict,
+				'key_list':			self.key_list,
 				}
 		torch.save(meta, save_path)
 		print('save point meta data into {}'.format(save_path))
 
-	# def load(self, filename):
-	# 	assert os.path.isfile(filename), '{} is not a file'.format(filename)
-	# 	checkpoint       = torch.load(filename)
-	# 	self.predictions = checkpoint['predictions']
-	# 	self.groundtruth = checkpoint['groundtruth']
-	# 	self.image_lists = checkpoint['image_lists']
-	# 	self.mae_bars    = checkpoint['mae_bars']
-	# 	self.spefic_errs = checkpoint['spefic_errs']
+	def load(self, save_path):
+		if self.debug:
+			assert is_path_exists(save_path), '{} is not a file'.format(save_path)
+
+		print('load point meta data from {}'.format(save_path))			
+		checkpoint       		= torch.load(save_path)
+		self.pts_root 			= checkpoint['pts_root']
+		self.pts_forward 		= checkpoint['pts_forward']
+		self.pts_backward 		= checkpoint['pts_backward']
+		self.pts_anno    		= checkpoint['pts_anno']
+		self.image_prev_path 	= checkpoint['image_prev_path']
+		self.image_next_path	= checkpoint['image_next_path']
+
+		self.length 			= checkpoint['length']
+		self.pts_valid_index 	= checkpoint['pts_valid_index']
+		self.index_dict 		= checkpoint['index_dict']
+		self.key_list 			= checkpoint['key_list']
 
 	def compute_valid_tracking(self, index, threshold=10):
 		'''
@@ -129,7 +139,7 @@ class eval_meta_tracking_pts():
 		if self.debug:
 			assert self.length > 0, 'no enough data'
 
-		N = 1
+		N = 1.0
 		success_rate = 0
 		for key in self.key_list:
 			if not key in self.pts_valid_index:				# if valid index has not already been computed
@@ -138,10 +148,10 @@ class eval_meta_tracking_pts():
 
 			valid_index_tmp = self.pts_valid_index[key]
 			num_pts_tmp = self.pts_forward[key].shape[1]
-			success_rate_tmp = valid_index_tmp * 1.0 / num_pts_tmp
+			success_rate_tmp = len(valid_index_tmp) * 1.0 / num_pts_tmp
 
 			success_rate = (N-1)/N * success_rate + success_rate_tmp / N
-			N += 1
+			N += 1.0
 
 		return success_rate
 
@@ -154,7 +164,7 @@ class eval_meta_tracking_pts():
 			assert self.length > 0, 'no enough data'
 			assert method == 'forward' or method == 'backward', 'the accuracy can only be computed based on forward with annotations or backward check'
 
-		N = 1
+		N = 1.0
 		average_error = 0
 		for key in self.key_list:
 			if not key in self.pts_valid_index:				# if valid index has not already been computed
@@ -176,132 +186,6 @@ class eval_meta_tracking_pts():
 				error_tmp, _ = pts_euclidean(pts_backward_valid, pts_root_valid, debug=self.debug)
 
 			average_error = (N-1)/N * average_error + error_tmp / N
-			N += 1
+			N += 1.0
 
 		return average_error
-
-  # def weight_mse(self, distances, avaliable):
-  #   num_point = distances.shape[1]
-  #   each_mses = np.zeros((num_point), dtype=float)
-  #   for i in range(num_point):
-  #     dis = distances[:, i]
-  #     dis = dis[avaliable[:,i]]
-  #     each_mses[i] = dis.mean()
-  #   return each_mses
-
-  # def compute_threshold(self, all_ava, all_mse, thresh):
-  #   all_ava = all_ava.astype('bool')
-  #   all_mse = all_mse.copy()
-  #   oks = all_mse < thresh
-  #   accuracy = (all_ava * oks).sum() * 1.0 / all_ava.sum()
-  #   all_mse[ all_mse >= thresh ] = thresh
-  #   avg = all_mse[ all_ava ].mean()
-  #   return accuracy, avg
-
-  # def specific_v0(self, avaliable, distances, log):
-  #   ## compute total average mse
-  #   num_points = distances.shape[1]
-  #   assert num_points == len(self.spefic_errs) or self.spefic_errs is None
-  #   scales = [1, 1.5, 2]
-
-  #   total_average_mse = distances * avaliable.astype('float')
-  #   print_log('Total  average mae : {:.2f}'.format(total_average_mse.sum()/avaliable.sum()) + ', Bars : {}  Scales : {}'.format(self.mae_bars, scales), log)
-  #   acc, trucavg = self.compute_threshold(avaliable, distances, 80)
-  #   print_log('Thresh average mae : {:.2f}, accuracy : {:5.2f}%'.format(trucavg, acc*100), log)
-
-  #   for i in range(num_points):
-  #     string = '--> {:2d}-th point || B'.format(i)
-  #     for bar in self.mae_bars:
-  #       acc, avg = self.compute_threshold(avaliable[:, i], distances[:, i], bar)
-  #       string = string + ' [{:4.1f}]=({:5.2f},{:5.1f}%)'.format(bar, avg, acc*100)
-
-  #     if self.spefic_errs is not None:      
-  #       string = string + ' || S'
-  #       for scale in scales:
-  #         bar = self.spefic_errs[i] * scale
-  #         acc, avg = self.compute_threshold(avaliable[:, i], distances[:, i], bar)
-  #         string = string + ' [{:4.1f}]=({:5.2f}, {:5.1f}%)'.format(bar, avg, acc*100)
-
-  #     print_log(string, log)
- 
-  #   return trucavg
-
-  # def get_scale_acc(self):
-  #   avaliable, distances = self.get_init()
-  #   num_point = self.predictions[0].shape[1]
-  #   p_erros = [28.13, 31.90, 26.32, 32.75, 14.16, 18.37, 11.89, 13.25, 13.06, 15.61, 13.54, 13.14, 17.63, 18.75, 13.57, 11.04, 12.76, 16.08, 7.88, 9.2]
-  #   scales = [0.5, 1, 1.5, 2]
-  #   scale_acc = np.zeros((len(scales), num_point), dtype='float32')
-  #   for j, scale in enumerate(scales):
-  #     for i in range(num_point):
-  #       ava, dis = avaliable[:, i], distances[:, i]
-  #       dis = dis[ava]
-  #       scale_acc[j, i] = (dis < p_erros[i]*scales[j]).mean()
-  #   return scale_acc
-
-  # def get_avg_mae(self, thresh, points):
-  #   avaliable, distances = self.get_init()
-  #   _, tavg = self.compute_threshold(avaliable[:,points], distances[:,points], thresh)
-  #   return tavg
-
-  # def get_init(self, is_x_axis=None):
-  #   assert (not self.predictions) == False, 'list is empty'
-  #   num_image = len(self.predictions)
-  #   num_point = self.predictions[0].shape[1]
-    
-  #   avaliable = np.zeros((num_image, num_point), dtype=bool)
-  #   distances = np.zeros((num_image, num_point), dtype=float)
-  #   for idx in range(num_image):
-  #     gt = self.groundtruth[idx]
-  #     xx = self.predictions[idx]
-  #     avaliable[idx, :] = gt[2, :].astype('bool')
-  #     xx = np.square(gt[:2,:] - xx[:2,:])
-  #     if is_x_axis is None:
-  #       distances[idx, :] = np.sqrt(xx[0] + xx[1])
-  #     elif is_x_axis:
-  #       distances[idx, :] = np.sqrt(xx[0])
-  #     else:
-  #       distances[idx, :] = np.sqrt(xx[1])
-        
-  #   return avaliable, distances
-
-  # def general_output_mse(self, avaliable, distances, log):
-  #   ## compute total average mse
-  #   num_points = distances.shape[1]
-
-  #   total_average_mse = distances * avaliable.astype('float')
-  #   print_log('Total  average mae : {:.2f}'.format(total_average_mse.sum()/avaliable.sum()) + ', Bars : {}'.format(self.mae_bars), log)
-  #   acc, trucavg = self.compute_threshold(avaliable, distances, 80)
-  #   print_log('Thresh average mae : {:.2f}, accuracy : {:5.2f}%'.format(trucavg, acc*100), log)
-
-  #   for i in range(num_points):
-  #     string = '--> {:2d}-th point || '.format(i)
-  #     for bar in self.mae_bars:
-  #       acc, avg = self.compute_threshold(avaliable[:, i], distances[:, i], bar)
-  #       string = string + ' [{:4.1f}]=({:5.2f},{:5.1f}%)'.format(bar, avg, acc*100)
-
-  #     print_log(string, log)
- 
-  #   return trucavg
-
-  # def compute_mse(self, log):
-  #   avaliable, distances = self.get_init()
-  #   num_image = len(self.predictions)
-  #   num_point = self.predictions[0].shape[1]
-  #   print_log('compute mse {} examples with {} points'.format(num_image, num_point), log)
-  #   mae = self.general_output_mse(avaliable, distances, log)
-  #   return mae
-  
-  # def calculate_acc(self, errors, points):
-  #   avaliable, distances = self.get_init()
-  #   assert isinstance(errors, list)
-  #   assert isinstance(points, list)
-  #   accuracies = np.zeros((len(errors), len(points)), dtype='float32')
-
-  #   for i in range(len(points)):
-  #     ava, dis = avaliable[:, points[i]], distances[:,points[i]]
-  #     dis = dis[ava]
-  #     for j, error in enumerate(errors):
-  #       accuracies[j, i] = (dis < error).mean()
-        
-  #   return accuracies
