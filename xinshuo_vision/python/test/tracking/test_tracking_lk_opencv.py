78c173611a12a4179fa868bbe361a091807ac1f0
xinshuoweng <xinshuo.weng@gmail.com>
2018-05-11 16:24:05 -0400

    upda tracking opencv

--- xinshuo_vision/python/test/tracking/test_tracking_lk_opencv.py
+++ xinshuo_vision/python/test/tracking/test_tracking_lk_opencv.py
@@ -4,16 +4,42 @@ import numpy as np, cv2
 
 import init_paths
 from xinshuo_io import load_image
-from xinshuo_visualization import visualize_image_with_pts
+from xinshuo_images import rgb2gray
+from xinshuo_visualization import visualize_image_with_pts, visualize_image, visualize_pts_array
+from xinshuo_miscellaneous import find_unique_common_from_lists
 from tracking import tracking_lk_opencv
 
 def test_tracking_lk_opencv():
 	print('test color image with backward')
 	image_prev = load_image('../frame_prev.jpg')
 	image_next = load_image('../frame_next.jpg')
-	input_pts = np.random.rand(2, 40)
-	input_pts *= 260
-	pts_forward, pts_bacward, backward_err = tracking_lk_opencv(image_prev, image_next, input_pts, backward=True)
+	num_pts = 300000
+	threshold = 0.05
+	input_pts = np.random.rand(2, num_pts)
+	input_pts *= 360
+	pts_forward, pts_bacward, backward_err, found_index = tracking_lk_opencv(image_prev, image_next, input_pts, win_size=15, backward=True)
+
+	assert len(backward_err) == num_pts
+	backward_err = np.array(backward_err).reshape((num_pts, 1))
+	err_small_index = np.where(backward_err[:, 0] < threshold)[0].tolist()
+	print('length of found index is %d' % len(found_index))
+	print('length of backward err is %d' % len(err_small_index))
+	good_index = find_unique_common_from_lists(err_small_index, found_index)
+	print('length of good index is %d' % (len(good_index)))
+	pts_good = input_pts[:, good_index]
+	pts_forward = pts_forward[:, good_index]
+	pts_bacward = pts_bacward[:, good_index]
+
+	print('test color image with forward')
+	pts_forward, _, _, found_index = tracking_lk_opencv(image_prev, image_next, pts_good)
+	visualize_image_with_pts(image_prev, pts_good, pts_size=2, color_index=2, vis=True, save_path='./0prev.jpg')
+	visualize_image_with_pts(image_next, pts_forward, pts_size=2, color_index=2, vis=True, save_path='./1next.jpg')
+	visualize_image_with_pts(image_prev, pts_bacward, pts_size=2, color_index=2, vis=True, save_path='./2back.jpg')
+
+	print('test grayscale image with forward')
+	pts_forward, _, _, found_index = tracking_lk_opencv(rgb2gray(image_prev), image_next, pts_good)
+	visualize_image_with_pts(image_prev, pts_good, pts_size=2, color_index=2, vis=True)
+	visualize_image_with_pts(image_next, pts_forward, pts_size=2, color_index=2, vis=True)
 
 	print('\n\nDONE! SUCCESSFUL!!\n')
 	
--- xinshuo_vision/python/tracking.py
+++ xinshuo_vision/python/tracking.py
@@ -7,7 +7,7 @@ import cv2, numpy as np
 from xinshuo_images.python.private import safe_image
 from xinshuo_math.python.private import safe_2dptsarray
 from xinshuo_images import rgb2gray
-from xinshuo_miscellaneous import iscolorimage_dimension, isscalar
+from xinshuo_miscellaneous import iscolorimage_dimension, isscalar, find_unique_common_from_lists
 from xinshuo_math import pts_euclidean
 
 def tracking_lk_opencv(input_image1, input_image2, input_pts, backward=False, win_size=15, pyramid=5, warning=True, debug=True):
@@ -26,6 +26,7 @@ def tracking_lk_opencv(input_image1, input_image2, input_pts, backward=False, wi
 		pts_forward:		tracked points in forward pass, 2 x N float32 numpy
 		pts_bacward:		tracked points in backward pass, 2 x N float32 numpy, None is not runnign the backward pass
 		backward_err_list:	a list of error in forward-backward pass check, None if not running the backward pass
+		found_index_list:	a list of 0 or 1, 1 if the tracking converges, 0 if not converging
 	'''
 	np_image1, _ = safe_image(input_image1, warning=warning, debug=debug)
 	np_image2, _ = safe_image(input_image2, warning=warning, debug=debug)
@@ -37,16 +38,22 @@ def tracking_lk_opencv(input_image1, input_image2, input_pts, backward=False, wi
 	if iscolorimage_dimension(np_image1): np_image1 = rgb2gray(np_image1)
 	if iscolorimage_dimension(np_image2): np_image2 = rgb2gray(np_image2)
 
-	lk_params = dict(winSize=(win_size, win_size), maxLevel=pyramid, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+	lk_params = dict(winSize=(win_size, win_size), maxLevel=pyramid, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10000, 0.03))
 	pts_root = np.expand_dims(np_pts.transpose(), axis=1) 			# N x 1 x 2
 	
 	pts_forward, status_for, err_for = cv2.calcOpticalFlowPyrLK(np_image1, np_image2, pts_root, None, **lk_params) 
+	found_index_for = np.where(status_for[:, 0] == 1)[0].tolist()
 	if backward: 
 		pts_bacward, status_bac, err_bac = cv2.calcOpticalFlowPyrLK(np_image2, np_image1, pts_forward, None, **lk_params)
+		# print(status_bac)
+		found_index_bac = np.where(status_bac[:, 0] == 1)[0].tolist()
+		# aa
 		pts_bacward = pts_bacward.reshape((-1, 2)).transpose()
 		_, backward_err_list = pts_euclidean(np_pts, pts_bacward, warning=warning, debug=debug)
+		found_index_list = find_unique_common_from_lists(found_index_for, found_index_bac, warning=warning, debug=debug)
 	else: 
 		pts_bacward, backward_err_list = None, None
+		found_index_list = found_index_for
 
 	pts_forward = pts_forward.reshape((-1, 2)).transpose()			#  2 x N
-	return pts_forward, pts_bacward, backward_err_list
+	return pts_forward, pts_bacward, backward_err_list, found_index_list
