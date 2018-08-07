d6b52e6d17390c7b8bf0aff902a39c59f2cf61d7
xinshuoweng <xinshuo.weng@gmail.com>
2018-08-07 19:42:40 -0400

    update toolbox

--- xinshuo_vision/python/camera_geometry.py
+++ xinshuo_vision/python/camera_geometry.py
@@ -1,16 +1,32 @@
 # Author: Xinshuo Weng
 # Email: xinshuow@cs.cmu.edu
 import numpy as np
+from xinshuo_math.python.private import safe_2dptsarray
 from xinshuo_math import pts_euclidean
+from xinshuo_miscellaneous import is2dptsarray_confidence, is2dptsarray_occlusion, is2dptsarray
 
-def triangulate_two_views(pts_array1, pts_array2, projection1, projection2, warning=True, debug=True):
+def triangulate_two_views(input_pts1, input_pts2, projection1, projection2, scaling_factor=1, warning=True, debug=True):
+	'''
+	triangulation from two views
+	'''
 	#       projection1 - 3 x 4 Camera Matrix 1
 	#       pts_array1 - 3 x N set of points
 	#       projection2 - 3 x 4 Camera Matrix 2
 	#       pts_array2 - 3 x N set of points
+	try: pts_array1 = safe_2dptsarray(input_pts1, homogeneous=True, warning=warning, debug=debug)
+	except AssertionError: pts_array1 = safe_2dptsarray(input_pts1, homogeneous=False, warning=warning, debug=debug)
+	if debug: assert is2dptsarray(pts_array1) or is2dptsarray_occlusion(pts_array1) or is2dptsarray_confidence(pts_array1), 'first input points are not correct'
+	try: pts_array2 = safe_2dptsarray(input_pts2, homogeneous=True, warning=warning, debug=debug)
+	except AssertionError: pts_array2 = safe_2dptsarray(input_pts2, homogeneous=False, warning=warning, debug=debug)
+	if debug: assert is2dptsarray(pts_array2) or is2dptsarray_occlusion(pts_array2) or is2dptsarray_confidence(pts_array2), 'second input points are not correct'
 
 	# initialization
 	num_pts = pts_array1.shape[1]
+	pts_array1[0:2, :] = pts_array1[0:2, :] / float(scaling_factor)
+	pts_array2[0:2, :] = pts_array2[0:2, :] / float(scaling_factor)
+	# print(pts_array1)
+	# print(pts_array2)
+	# aa
 
 	# least square
 	p1T1 = projection1[0, :]		# 1 x 4
@@ -19,25 +35,32 @@ def triangulate_two_views(pts_array1, pts_array2, projection1, projection2, warn
 	p2T1 = projection2[0, :]
 	p2T2 = projection2[1, :]
 	p2T3 = projection2[2, :]
+
+	print(projection1)
+	print(projection2)
+
 	# A = [projection1; projection2];
 	# H = (A'*A)\A';
 	pts_3d = np.zeros((num_pts, 4), 'float32')			# N x 4
 	pts_3d.fill(-1)
 	p1_proj = pts_array1.copy().transpose()				# N x 3
 	p2_proj = pts_array2.copy().transpose()
-	error = 0
 	for i in range(num_pts):
 		if pts_array1[2, i] == -1 or pts_array2[2, i] == -1: continue
 		# print(pts_array1[1, i].shape)
 		# print(p1T3.shape)
 		U = np.zeros((6, 4), dtype='float32')			# 6 x 4
 
-		U[0, :] = np.multiply(pts_array1[1, i], p1T3) - p1T2
-		U[1, :] = p1T1 - np.multiply(pts_array1[0, i], p1T3)
-		U[2, :] = np.multiply(pts_array1[0, i], p1T2) - np.multiply(pts_array1[1, i], p1T1)
-		U[3, :] = np.multiply(pts_array2[1, i], p2T3) - p2T2
-		U[4, :] = p2T1 - np.multiply(pts_array2[0, i], p2T3)
-		U[5, :] = np.multiply(pts_array2[0, i], p2T2) - np.multiply(pts_array2[2, i], p2T1)
+		U[0, :] = np.multiply(pts_array1[1, i], p1T3) - p1T2									# y * p1T3 - p1T2
+		U[1, :] = p1T1 - np.multiply(pts_array1[0, i], p1T3)									# p1T1 - x * p1T3
+		U[2, :] = np.multiply(pts_array1[0, i], p1T2) - np.multiply(pts_array1[1, i], p1T1)		# x * p1T2 - y * p1T1
+		U[3, :] = np.multiply(pts_array2[1, i], p2T3) - p2T2									# y * p2T3 - p2T2
+		U[4, :] = p2T1 - np.multiply(pts_array2[0, i], p2T3)									# p2T1 - x * p2T3
+		U[5, :] = np.multiply(pts_array2[0, i], p2T2) - np.multiply(pts_array2[1, i], p2T1)		# x * p2T2 - y * p2T1
+
+		print(U)
+		aa
+
 		_, _, V = np.linalg.svd(U)
 		pts_3d[i, :] = V[:, -1].transpose()
 
@@ -57,7 +80,7 @@ def triangulate_two_views(pts_array1, pts_array2, projection1, projection2, warn
 		# error = error + np.norm(p1_proj[i, 0:2] - pts_array1[0:2, i]) + np.norm(p2_proj[i, 0:2] - pts_array2[0:2, i])
 
 	error_tmp, _ = pts_euclidean(p1_proj[:, 0:2].transpose(), pts_array1[0:2, :], warning=warning, debug=debug)
-	error += error_tmp * num_pts
+	error = error_tmp * num_pts
 	error_tmp, _ = pts_euclidean(p2_proj[:, 0:2].transpose(), pts_array2[0:2, :], warning=warning, debug=debug)
 	error += error_tmp * num_pts
 
--- xinshuo_vision/python/test/camera_geometry/test_camera_geometry.py
+++ xinshuo_vision/python/test/camera_geometry/test_camera_geometry.py
@@ -5,7 +5,12 @@ import numpy as np
 import init_paths
 from mycamera import MyCamera, load_camera_cluster
 from camera_geometry import triangulate_two_views
-from xinshuo_io import anno_parser
+from xinshuo_io import anno_parser, load_image
+from xinshuo_visualization import visualize_image_with_pts, visualize_pts_array
+
+
+# check the projection matrix
+
 
 def test_triangulate_two_views():
 	calibration_file = '../calibration.txt'
@@ -19,10 +24,15 @@ def test_triangulate_two_views():
 	# print(pts_array2)
 	projection1 = camera_cluster['400015'].get_projection_matrix()
 	projection2 = camera_cluster['400053'].get_projection_matrix()
-	pts_3d, pts1_reproj, pts2_reproj = triangulate_two_views(pts_array1, pts_array2, projection1, projection2)
-	print(pts_3d)
+	pts_3d, pts1_reproj, pts2_reproj = triangulate_two_views(pts_array1, pts_array2, projection1, projection2, scaling_factor=1000)
+	print(pts_array1)
+	print(pts1_reproj)
 
-	fig, ax = visualization_image_w
+	img_path = '../20180514--handsy--400015--0800.png'
+	img = load_image(img_path)
+	debug = False
+	fig, ax = visualize_image_with_pts(img, pts_array1, debug=debug, closefig=False)
+	visualize_pts_array(pts1_reproj, fig=fig, ax=ax, color_index=1, pts_size=20, label=True, vis=True, debug=debug)
 
 	print('\n\nDONE! SUCCESSFUL!!\n')
 	
